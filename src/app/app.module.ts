c6ec6c9e1056c8431228b32e444371b6fd0ced7d
Daniel Berger <daniel.berger95@gmail.com>
2020-09-23 08:38:48 -0400

    Added card-container component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CardContainerComponent } from './card-container/card-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    CardContainerComponent
   ],
   imports: [
     BrowserModule
