6ce25127fa08602271857c880e0033dfb849964b
Daniel Berger <daniel.berger95@gmail.com>
2020-09-30 11:48:40 -0400

    Added testing to transfer service

--- src/app/services/transfer.service.spec.ts
+++ src/app/services/transfer.service.spec.ts
@@ -1,8 +1,10 @@
+import { Transfer } from "./../models/transfer.model";
 import { TestBed } from "@angular/core/testing";
+import { FormControl, FormGroup } from "@angular/forms";
 
 import { TransferService } from "./transfer.service";
 
-xdescribe("TransferService", () => {
+describe("TransferService", () => {
   let service: TransferService;
 
   beforeEach(() => {
@@ -12,5 +14,32 @@ xdescribe("TransferService", () => {
 
   it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+  it("should return a transfer observable", () => {
+    const transfer$ = service.getTransfer();
+    expect(transfer$).toBeTruthy();
+    expect(transfer$).toEqual(service.transfer$);
+  });
+
+  it("should update transfer observable", () => {
+    let transferForm = new FormGroup({
+      toAccount: new FormControl("test"),
+      amount: new FormControl(0),
+    });
+    service.openConfirm("fromAccount", "toAccount", 100, transferForm);
+    let transfer: Transfer;
+    service.transfer$.subscribe((res) => (transfer = res));
+    expect(transfer).toBeTruthy();
+    expect(transfer).toEqual({
+      fromAccount: "fromAccount",
+      toAccount: "toAccount",
+      amount: 100,
+    });
+  });
+  it("should empty the transfer observable", () => {
+    service.closeConfirm();
+    let transfer: Transfer;
+    service.transfer$.subscribe((res) => (transfer = res));
+    expect(transfer).toBeFalsy();
   });
 });
